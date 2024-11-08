316b558fc0e686a98147462f904bedc5719b38a5
Matthew <matthew.asher941@gmail.com>
2024-11-08 02:46:53 -0800

    Set up PostgreSQL database and initial models

--- movie/settings.py
+++ movie/settings.py
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movie_db',         
+        'USER': 'postgres',          
+        'PASSWORD': 'movieproj',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
