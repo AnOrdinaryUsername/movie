5e5a8869589a85add11dad1aa18f465eaa3649d4
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-11-13 17:56:10 -0800

    Create api docs using drf-yasg

--- movie/urls.py
+++ movie/urls.py
@@ -15,18 +15,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-from api import views
 
-router = routers.DefaultRouter()
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Reel Reviews API",
+      default_version='v1',
+      description="Reel Reviews is a place to create and share movie reviews.",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "api/v1/docs/swagger",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "api/v1/docs/redoc/", 
+         schema_view.with_ui("redoc", cache_timeout=0), 
+         name="schema-redoc"
+    ),
     path("api/v1/", include("api.urls")),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.authtoken")),
