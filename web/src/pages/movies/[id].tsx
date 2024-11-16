21b56178c05826a3c13bc93f7dee4c37ea06247a
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-11-16 11:35:41 -0800

    Allow users to create a review for a movie

--- web/src/pages/movies/[id].tsx
+++ web/src/pages/movies/[id].tsx
@@ -2,22 +2,44 @@ import GenericLayout from '@/components/Layouts';
 import Header from '@/components/Header';
 import {
   Badge,
+  Button,
   Card,
   Container,
   Group,
   Image,
+  Modal,
   rem,
   SimpleGrid,
   Stack,
   Text,
+  Textarea,
   Title,
 } from '@mantine/core';
 import Link from 'next/link';
 
 import type { GetServerSidePropsContext } from 'next';
 import type { MovieInfo, Genre } from '@/types';
+import { useDisclosure } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
 
-type Props = MovieInfo;
+
+interface Review {
+  id: string;
+  user: number;
+  movie: string;
+  content: string;
+}
+
+interface User {
+  username: string;
+  id: number;
+}
+
+interface Props extends MovieInfo {
+  movie_id: string;
+  user: User;
+  reviews: Array<Review>;
+}
 
 export default function MoviesPage({
   genres,
@@ -26,7 +48,65 @@ export default function MoviesPage({
   media_length,
   media_description,
   image_url,
+  movie_id,
+  reviews
 }: Props) {
+  const [user, setUser] = useState<User | null>(null);
+  const [opened, { open, close }] = useDisclosure(false);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    let user: User | null = null;
+    
+    if (token !== null) {
+      const auth = {
+        'Authorization': `Token ${token}`
+      }
+
+      fetch(`http://localhost:8000/api/v1/auth/users/me`, {
+        headers: new Headers(auth)
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data)
+      });
+    }
+  }, []);
+
+  async function submitReview() {
+    const token = sessionStorage.getItem('token');
+    console.log(user);
+  
+    if (token !== null && user !== null) {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${token}`
+      }
+
+      const body = {
+        user: user.id,
+        movie: movie_id,
+        content: text,
+      }
+
+      const newReview = await fetch(`http://localhost:8000/api/v1/movies/${movie_id}/reviews`, {
+        method: 'POST',
+        headers: new Headers(headers),
+        body: JSON.stringify(body)
+      });
+
+      try {
+        if (newReview.status !== 201) {
+          throw new Error("Unable to create a review.")
+        }
+        console.log(`${user.username} created a review for ${media_title}.`)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+
   return (
     <GenericLayout size="lg" bg="#dce4f5">
       <Header />
@@ -83,7 +163,37 @@ export default function MoviesPage({
               </Stack>
             ))}
           </Group>
+          <Group justify="space-between" w="100%" pb={rem(16)}>
+            <Title order={2}>Reviews</Title>
+            {user && <Button onClick={open}>Write a Review</Button>}
+          </Group>
         </Stack>
+        <Stack>
+            {reviews && reviews.map((review) => (
+              <Stack key={review.id}>
+                <Card
+                  p={rem(16)}
+                  radius="md"
+                  w="100%"
+                >
+                  <Title order={3} pt={rem(16)}>
+                    {review.user}
+                  </Title>
+                  <Text>{review.content}</Text>
+                </Card>
+              </Stack>
+            ))}
+          </Stack>
+        <Modal centered opened={opened} onClose={close} title="My Review">
+          <Textarea 
+            resize="vertical" 
+            label="Review" 
+            placeholder="This movie is amazing!"
+            value={text}
+            onChange={(event) => setText(event.currentTarget.value)}
+          />
+          <Button onClick={submitReview}>Submit</Button>
+        </Modal>
       </Container>
     </GenericLayout>
   );
@@ -104,6 +214,23 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       console.error(error);
     });
 
+
+  let reviews: Array<Review> | null = null;
+
+  try {
+    const movieReviews = await fetch(`http://localhost:8000/api/v1/movies/${id}/reviews`);
+
+    if (!movieReviews.ok) {
+      throw new Error("Reviews did not return a 200 response")
+    }
+
+    reviews = await movieReviews.json();
+
+  } catch (error) {
+    console.error(error);
+  }
+
+
   const { genres, actors_list, media_title, media_length, media_description, image_url } = result;
 
   return {
@@ -114,6 +241,8 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       media_length,
       media_description,
       image_url,
+      reviews,
+      movie_id: id,
     },
   };
 }
