7cd0567fd8c2942c6c1462f8496c7a7222f84088
AnOrdinaryUsername <57053268+AnOrdinaryUsername@users.noreply.github.com>
2024-11-29 00:48:52 -0800

    Customize responses for ReviewList/ReviewDetail
    IsOwnerOrReadOnly checks if user is the owner of a review and
    returns a 403 if not. Additional checks, have been made to ensure
    that no more than 1 review exists per movie from a user.

--- api/views.py
+++ api/views.py
@@ -2,8 +2,13 @@ from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
-from rest_framework.authentication import get_authorization_header
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    AllowAny,
+    BasePermission,
+    SAFE_METHODS,
+)
+from rest_framework.authentication import get_authorization_header, TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework import generics, status
 from rest_framework.response import Response
@@ -17,6 +22,22 @@ from api.serializers import MovieInfoSerializer, ReviewSerializer
 User = get_user_model()
 
 
+class IsOwnerOrReadOnly(BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        print(obj.user)
+        return obj.user == request.user
+
+
 class MovieInfoList(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = MovieInfo.objects.all()
@@ -25,6 +46,7 @@ class MovieInfoList(generics.ListCreateAPIView):
 
 class MovieInfoDetail(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = MovieInfo.objects.all()
     serializer_class = MovieInfoSerializer
 
     def get_object(self, queryset=None):
@@ -33,6 +55,7 @@ class MovieInfoDetail(generics.RetrieveAPIView):
 
 class UserInfoDetail(generics.RetrieveAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = CustomUser.objects.all()
     serializer_class = UserSerializer  # Now properly imported
 
     def get_object(self, queryset=None):
@@ -47,14 +70,70 @@ class ReviewList(generics.ListCreateAPIView):
         movie_id = self.kwargs["pk"]
         return Review.objects.filter(movie=movie_id)
 
+    def create(self, request, *args, **kwargs):
+        movie_id = self.kwargs["pk"]
 
-class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+        try:
+            review = Review.objects.get(movie=movie_id, user=request.user)
+            if review:
+                return Response(
+                    {"error": "Review already exists"},
+                    status=status.HTTP_409_CONFLICT,
+                )
+        except:
+            pass
 
-    def get_object(self, queryset=None):
-        return Review.objects.get(pk=self.kwargs["pk"])
+        new_review = Review.objects.create(
+            user=request.user,
+            movie=MovieInfo.objects.get(id=movie_id),
+            content=request.data.get("content"),
+        )
+        serializer = ReviewSerializer(new_review)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ReviewDetail(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    
+    def get(self, request, movie_id, pk):
+        try:
+            review = Review.objects.get(id=pk)
+            self.check_object_permissions(request, review)
+        except Review.DoesNotExist:
+            return Response({ "error": "Review not found" }, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+
+    def patch(self, request, movie_id, pk):
+        content = request.data.get("content")
+
+        if not content:
+            return Response({ "error": "content is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            review = Review.objects.get(id=pk)
+            self.check_object_permissions(request, review)
+        except Review.DoesNotExist:
+            return Response({ "error": "Review not found" }, status=status.HTTP_404_NOT_FOUND)
+
+        res = Review.objects.filter(id=pk).update(
+            content=content
+        )
+
+        return Response(res)
+    
+    def delete(self, request, movie_id, pk):
+        try:
+            review = Review.objects.get(id=pk)
+            self.check_object_permissions(request, review)
+        except Review.DoesNotExist:
+            return Response({ "error": "Review not found" }, status=status.HTTP_404_NOT_FOUND)
+        
+        review.delete()
+
+        return Response({ "message": "Deleted review" }, status=status.HTTP_204_NO_CONTENT)
 
 
 class MovieSearchView(APIView):
@@ -92,54 +171,37 @@ class UserFavoriteList(APIView):
     @swagger_auto_schema(
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
-            required=['movie_id'],
+            required=["movie_id"],
             properties={
-                'movie_id': openapi.Schema(type=openapi.FORMAT_UUID),
-            }
+                "movie_id": openapi.Schema(type=openapi.FORMAT_UUID),
+            },
         ),
         responses={
             201: openapi.Response(
                 description="CREATED",
-                examples={
-                    "application/json": {
-                    'message': 'Movie added to favorites'
-                    }
-                }
+                examples={"application/json": {"message": "Movie added to favorites"}},
             ),
             400: openapi.Response(
                 description="BAD REQUEST",
-                examples={
-                    "application/json": {
-                    "error": "movie_id is required"
-                    }
-                }
+                examples={"application/json": {"error": "movie_id is required"}},
             ),
             401: openapi.Response(
                 description="UNAUTHORIZED",
-                examples={
-                    "application/json": {
-                    "error": "Invalid token"
-                    }
-                }
+                examples={"application/json": {"error": "Invalid token"}},
             ),
             404: openapi.Response(
                 description="NOT FOUND",
                 examples={
                     "application/json": {
-                    "detail": "No CustomUser matches the given query."
+                        "detail": "No CustomUser matches the given query."
                     }
-                }
+                },
             ),
-            
             409: openapi.Response(
                 description="CONFLICT",
-                examples={
-                    "application/json": {
-                    "error": "Movie already in favorites"
-                    }
-                }
+                examples={"application/json": {"error": "Movie already in favorites"}},
             ),
-        }
+        },
     )
     def post(self, request, pk):
         # Get token from Authorization header
@@ -178,45 +240,31 @@ class UserFavoriteList(APIView):
     @swagger_auto_schema(
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
-            required=['movie_id'],
+            required=["movie_id"],
             properties={
-                'movie_id': openapi.Schema(type=openapi.FORMAT_UUID),
-            }
+                "movie_id": openapi.Schema(type=openapi.FORMAT_UUID),
+            },
         ),
         responses={
             204: openapi.Response(
                 description="NO CONTENT",
                 examples={
-                    "application/json": {
-                    'message': 'Movie removed from favorites'
-                    }
-                }
+                    "application/json": {"message": "Movie removed from favorites"}
+                },
             ),
             400: openapi.Response(
                 description="BAD REQUEST",
-                examples={
-                    "application/json": {
-                    "error": "movie_id is required"
-                    }
-                }
+                examples={"application/json": {"error": "movie_id is required"}},
             ),
             401: openapi.Response(
                 description="UNAUTHORIZED",
-                examples={
-                    "application/json": {
-                    "error": "Invalid token"
-                    }
-                }
+                examples={"application/json": {"error": "Invalid token"}},
             ),
             404: openapi.Response(
                 description="NOT FOUND",
-                examples={
-                    "application/json": {
-                    "error": "Movie not in favorites"
-                    }
-                }
+                examples={"application/json": {"error": "Movie not in favorites"}},
             ),
-        }
+        },
     )
     def delete(self, request, pk):
         # Get token from Authorization header
@@ -252,6 +300,7 @@ class UserFavoriteList(APIView):
                 {"error": "Movie not in favorites"}, status=status.HTTP_404_NOT_FOUND
             )
 
+
 class UserWatchList(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
@@ -265,54 +314,37 @@ class UserWatchList(APIView):
     @swagger_auto_schema(
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
-            required=['movie_id'],
+            required=["movie_id"],
             properties={
-                'movie_id': openapi.Schema(type=openapi.FORMAT_UUID),
-            }
+                "movie_id": openapi.Schema(type=openapi.FORMAT_UUID),
+            },
         ),
         responses={
             201: openapi.Response(
                 description="CREATED",
-                examples={
-                    "application/json": {
-                    'message': 'Movie added to watchlist'
-                    }
-                }
+                examples={"application/json": {"message": "Movie added to watchlist"}},
             ),
             400: openapi.Response(
                 description="BAD REQUEST",
-                examples={
-                    "application/json": {
-                    "error": "movie_id is required"
-                    }
-                }
+                examples={"application/json": {"error": "movie_id is required"}},
             ),
             401: openapi.Response(
                 description="UNAUTHORIZED",
-                examples={
-                    "application/json": {
-                    "error": "Invalid token"
-                    }
-                }
+                examples={"application/json": {"error": "Invalid token"}},
             ),
             404: openapi.Response(
                 description="NOT FOUND",
                 examples={
                     "application/json": {
-                    "detail": "No CustomUser matches the given query."
+                        "detail": "No CustomUser matches the given query."
                     }
-                }
+                },
             ),
-            
             409: openapi.Response(
                 description="CONFLICT",
-                examples={
-                    "application/json": {
-                    "error": "Movie already in watchlist"
-                    }
-                }
+                examples={"application/json": {"error": "Movie already in watchlist"}},
             ),
-        }
+        },
     )
     def post(self, request, pk):
         # Get token from Authorization header
@@ -351,45 +383,31 @@ class UserWatchList(APIView):
     @swagger_auto_schema(
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
-            required=['movie_id'],
+            required=["movie_id"],
             properties={
-                'movie_id': openapi.Schema(type=openapi.FORMAT_UUID),
-            }
+                "movie_id": openapi.Schema(type=openapi.FORMAT_UUID),
+            },
         ),
         responses={
             204: openapi.Response(
                 description="NO CONTENT",
                 examples={
-                    "application/json": {
-                    'message': 'Movie removed from watchlist'
-                    }
-                }
+                    "application/json": {"message": "Movie removed from watchlist"}
+                },
             ),
             400: openapi.Response(
                 description="BAD REQUEST",
-                examples={
-                    "application/json": {
-                    "error": "movie_id is required"
-                    }
-                }
+                examples={"application/json": {"error": "movie_id is required"}},
             ),
             401: openapi.Response(
                 description="UNAUTHORIZED",
-                examples={
-                    "application/json": {
-                    "error": "Invalid token"
-                    }
-                }
+                examples={"application/json": {"error": "Invalid token"}},
             ),
             404: openapi.Response(
                 description="NOT FOUND",
-                examples={
-                    "application/json": {
-                    "error": "Movie not in watchlist"
-                    }
-                }
+                examples={"application/json": {"error": "Movie not in watchlist"}},
             ),
-        }
+        },
     )
     def delete(self, request, pk):
         # Get token from Authorization header
